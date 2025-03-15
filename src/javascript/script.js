6d3fa2ef6e651c5c893b46a8cd9b3c5edb895d36
Joane <138986247+japcunha@users.noreply.github.com>
2025-03-14 22:00:04 -0300

    js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,7 +1,40 @@
-$(document).ready(function() {
-    $('#mobile_btn').on('click', function() {
-        $('#mobile_menu').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x');
+$(document).ready(function () {
+  $("#mobile_btn").on("click", function () {
+    $("#mobile_menu").toggleClass("active");
+    $("#mobile_btn").find("i").toggleClass("fa-x");
+  });
+  const sections = $("section");
+  const navItems = $(".nav-item");
 
-   });
+  $(window).on("scroll", function () {
+    const header = $("header");
+    const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+    let activeSectionIndex = 0;
+
+
+    if (scrollPosition <= 0) {
+      header.css("box-shadow", "none");
+    } else {
+      header.css("box-shadow", "5px 1px 5px rgba(0,0,0,0.1");
+    }
+
+    sections.each(function (index) {
+      const section = $(this);
+      const top = $(this).offset().top - 96;
+      const bottom = top + $(this).outerHeight();
+
+      if (scrollPosition >= top && scrollPosition <= bottom) {
+        activeSection = index;
+        return false;
+      }
+    });
+    $(navItems[activeSectionIndex]).addClass("active");
+
+    ScrollReveal().reveal("#cta", {
+      origin: "left",
+      duration: 2000,
+      distance: "20%",
+    });
+  });
 });
